92de252c307870f1992fd3b7fe502fe7a942dc32
David Griffon <dgriffon@jahia.com>
2022-03-30 19:11:59 +0200

    Revert "BACKLOG-16967 : html escape url in sitemap files"
    This reverts commit 751ae35c4c0e8b60b6b46ed386362b68de63d299.

--- src/main/java/org/jahia/modules/sitemap/utils/Utils.java
+++ src/main/java/org/jahia/modules/sitemap/utils/Utils.java
@@ -23,9 +23,7 @@
  */
 package org.jahia.modules.sitemap.utils;
 
-import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.jackrabbit.util.Text;
 import org.jahia.api.Constants;
 import org.jahia.modules.sitemap.beans.SitemapEntry;
 import org.jahia.registries.ServicesRegistry;
@@ -38,8 +36,6 @@ import org.jahia.services.seo.urlrewrite.ServerNameToSiteMapper;
 import org.jahia.services.sites.JahiaSitesService;
 import org.jahia.services.usermanager.JahiaUser;
 import org.jahia.settings.SettingsBean;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.jcr.NodeIterator;
 import javax.jcr.RepositoryException;
@@ -56,7 +52,6 @@ import java.util.stream.Collectors;
  */
 public final class Utils {
 
-    private static final Logger logger = LoggerFactory.getLogger(Utils.class);
     private static final String DEDICATED_SITEMAP_MIXIN = "jseomix:sitemapResource";
     private static final String NO_INDEX_MIXIN = "jseomix:noIndex";
 
@@ -78,7 +73,7 @@ public final class Utils {
             while (ni.hasNext()) {
                 JCRNodeWrapper n = (JCRNodeWrapper) ni.nextNode();
                 if (isValidEntry(n, renderContext)) {
-                    results.add(htmlEscape(n.getPath(), false));
+                    results.add(n.getPath());
                 }
             }
             return null;
@@ -129,12 +124,12 @@ public final class Utils {
                 }
                 JCRNodeWrapper nodeInOtherLocale = sessionInOtherLocale.getNode(node.getPath());
                 if (nodeInOtherLocale != null && isValidEntry(nodeInOtherLocale, renderContext)) {
-                    linksInOtherLanguages.add(new SitemapEntry(htmlEscape(nodeInOtherLocale.getPath(), false), htmlEscape(nodeInOtherLocale.getUrl(), true), new SimpleDateFormat("yyyy-MM-dd").format(node.getLastModifiedAsDate()), otherLocale, null));
+                    linksInOtherLanguages.add(new SitemapEntry(nodeInOtherLocale.getPath(), nodeInOtherLocale.getUrl(), new SimpleDateFormat("yyyy-MM-dd").format(node.getLastModifiedAsDate()), otherLocale, null));
                 }
                 return null;
             });
         }
-        return new SitemapEntry(htmlEscape(node.getPath(), false), htmlEscape(node.getUrl(), true), new SimpleDateFormat("yyyy-MM-dd").format(node.getLastModifiedAsDate()), currentLocale, linksInOtherLanguages);
+        return new SitemapEntry(node.getPath(), node.getUrl(), new SimpleDateFormat("yyyy-MM-dd").format(node.getLastModifiedAsDate()), currentLocale, linksInOtherLanguages);
     }
 
     private static boolean isValidEntry(JCRNodeWrapper node, RenderContext renderContext) throws RepositoryException {
@@ -176,14 +171,6 @@ public final class Utils {
         }
         request.setAttribute("jahiaSitemapSiteKey", siteKey);
         request.setAttribute("jahiaSitemapSiteLanguage", siteDefaultLanguage);
-    }
-
-    private static String htmlEscape(String path, boolean unescapeText) {
-        String toEncode = path;
-        if (unescapeText) {
-            toEncode = Text.unescape(path);
-        }
-        return StringEscapeUtils.escapeHtml(toEncode);
     }
 
 }
